da8d091a2d425324ada6c6c9bd7ead0bf3dcb3c1
Derek Zeng <derek.zeng@brandsource.com>
2018-02-02 09:35:53 -0800

    Update files

--- index.js
+++
@@ -1,1 +0,0 @@
-              console.log("Prettify this statement");
---
+++ index.ts
@@ -0,0 +1,12 @@
+interface ITest {
+  str: string;
+  num: number;
+}
+
+      // This code should be prettied
+      const testObj = {
+        str: "Hello",
+        num: 5
+      };
+
+console.log(testObj);
